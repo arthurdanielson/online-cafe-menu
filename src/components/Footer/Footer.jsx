2f3814fd07c58ba203873dd17766546a77d07840
Arthur Danielson <main.danielson@gmail.com>
2024-02-08 01:02:35 -0400

    Finished footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
     return (
         <>
-        
+            <footer className="fixed-bottom bg-body-tertiary text-center rounded-top mx-3 p-3">
+                <small className="text-body-secondary">&copy; Arthur Danielson</small>
+            </footer>
         </>
     )
 }
